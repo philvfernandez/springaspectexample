4c55560d69957d86954b52959d4190311c474f4e
Phil Fernandez <philvfernandez@gmail.com>
2022-09-15 17:42:26 -0600

    Initial add.

---
+++ src/main/java/com/example/SprigAspectExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigAspectExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprigAspectExampleApplication.class, args);
+    }
+
+}
